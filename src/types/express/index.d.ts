91cd5073a83e22fed4e870fd973a3eedfda6c67c
Billones142 <stefanomerinoderui@gmail.com>
2025-06-01 02:10:28 -0300

    Refactor user data handling and improve authentication logic
    - Updated the loginController to use a structured payload for JWT tokens.
    - Enhanced getUserProfilesController to fetch user profiles from the database.
    - Modified selectProfileController to retrieve profile data dynamically.
    - Improved userLoggedCheckMiddleware to directly assign verified user data to the request.
    - Updated the verifyUser function to return user data from the database.
    - Added Profile interface to better manage user profiles in the application.

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,8 +1,9 @@
 // types/express/index.d.ts
-import { User } from '../models/User'; // tu tipo o interfaz de usuario
+import { Profile, User } from '../models/User'; // tu tipo o interfaz de usuario
 
 declare module 'express-serve-static-core' {
   interface Request {
     user?: User;
+    profile?: Profile,
   }
 }
