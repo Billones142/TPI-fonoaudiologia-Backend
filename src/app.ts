11898cb37b2770e11f5ee91174e4f0db6a29f81c
Billones142 <84104251+Billones142@users.noreply.github.com>
2025-06-06 18:36:38 -0300

    /heatlhz

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,24 @@
-import express from "express";
-import cors from "cors";
-import apiRoutes from "./api/index";
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import apiRoutes from './api/index';
 
 const app = express();
+
+app.get('/healthz', (req: Request, res: Response) => { // para Render y cron-job.org
+  res.send('ok');
+});
+
 app.use(
   cors({
     credentials: true,
     origin: process.env.FRONTEND_HOST ?? /^https?:\/\/localhost(:\d+)?$/,
-  })
+  }),
 );
 
-app.use("/api/v1", apiRoutes);
+// Add cookie parser middleware
+app.use(cookieParser());
 
-export default app;
+app.use('/api/v1', apiRoutes);
+
+export default app
